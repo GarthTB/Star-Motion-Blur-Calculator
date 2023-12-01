413bf158bda0be1c7b8ed290ae7dba85606bf818
Garth <g-art-h@outlook.com>
2023-12-01 20:18:47 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Star_Motion_Blur_Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RBTime_Click(object sender, RoutedEventArgs e)
+        {
+            TBTime.IsReadOnly = true;
+            TBFWHM.IsReadOnly = false;
+            TextBlockFWHM.Text = "星点半径";
+        }
+
+        private void RBLength_Click(object sender, RoutedEventArgs e)
+        {
+            TBTime.IsReadOnly = false;
+            TBFWHM.IsReadOnly = true;
+            TextBlockFWHM.Text = "拖线长度";
+        }
+
+        private void ButtonAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("本程序由Garth制作。\n","关于",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+    }
+}
